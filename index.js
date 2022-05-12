fa9b9a1a34d82a65cfd9c8aca2ab596aaf6f8499
Jen Cho <99620655+choyoonme@users.noreply.github.com>
2022-05-11 23:49:38 -0400

    add provided README and require code in index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Employee = require('./lib/employee');
+const Engineer = require('./lib/engineer');
+const Manager = require('./lib/manager');
+const Intern = require('./lib/intern')
 
-const writer = require('./src/template.js');
-
-console.log(writer("Turtles Rock!", new Employee(5).getName()));
+const Writer = require('./src/template');
